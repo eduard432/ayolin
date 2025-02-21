a3f2cf687d1815c1a4f82facf3b4f039dbdd4dc3
Eduardo <juantokx@gmail.com>
2025-02-21 08:23:55 -0600

    Add key prop to ToolCard component in Tools section for improved rendering

--- src/app/new_dashboard/chatbot/[id]/edit/Tools.tsx
+++ src/app/new_dashboard/chatbot/[id]/edit/Tools.tsx
@@ -117,7 +117,7 @@ export const Tools = () => {
 			</section>
 			<section className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4" >
 				{toolsExample.map((tool) => (
-					<ToolCard tool={tool} />
+					<ToolCard key={tool.id} tool={tool} />
 				))}
 			</section>
 		</div>
