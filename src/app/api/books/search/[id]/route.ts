fb294ef892a6f5fab2738ab32c79e94ba4361e6d
eduard432 <juantokx@gmail.com>
2025-03-05 21:20:27 -0600

    Implement book chunk search with embedding query and error handling

--- src/app/api/books/search/[id]/route.ts
+++ src/app/api/books/search/[id]/route.ts
@@ -1,5 +1,7 @@
 import { getDatabase } from '@/lib/db'
-import { BookDb } from '@/types/Books'
+import { BookChunkDb, BookDb } from '@/types/Books'
+import { openai } from '@ai-sdk/openai'
+import { embed } from 'ai'
 import { ObjectId } from 'mongodb'
 import { NextRequest } from 'next/server'
 
@@ -7,12 +9,45 @@ export async function GET(
 	request: NextRequest,
 	{ params }: { params: Promise<{ id: string }> }
 ) {
-    const { id } = await params
-    const bookObjectId = new ObjectId(id)
+	try {
+		const { id } = await params
+		const q = request.nextUrl.searchParams.get('q')
 
-    const db = await getDatabase()
-    const bookCollection = db.collection<BookDb>('book_chunks')
+		if (!q) throw Error('Missing query...')
 
-    
+		const bookObjectId = new ObjectId(id)
 
+		const db = await getDatabase()
+		const bookChunksCollection = db.collection<BookDb>('book_chunks')
+
+		const { embedding } = await embed({
+			model: openai.embedding('text-embedding-3-large'),
+			value: q,
+		})
+
+		const results: BookChunkDb[] = await bookChunksCollection
+			.aggregate<BookChunkDb>([
+				{
+					$match: {
+						bookId: bookObjectId,
+					},
+				},
+				{
+					$vectorSearch: {
+						index: 'embedding_index',
+						path: 'embedding',
+						queryVector: embedding,
+						numCandidates: 100,
+						limit: 5,
+					},
+				},
+			])
+			.toArray()
+
+		return {
+			results,
+		}
+	} catch (error) {
+		console.log(error)
+	}
 }
