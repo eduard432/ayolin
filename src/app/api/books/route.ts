d5f340ec9aa7f072792a8f0e678ca64c80138af8
eduard432 <juantokx@gmail.com>
2025-03-15 17:14:58 -0600

    Refactor book processing logic to accept cloudinaryId and pages as parameters; add new Cloudinary signature endpoint for secure uploads

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,13 +1,13 @@
-import { generateMd } from '@/ai/utils/books';
-import { uploadFile } from '@/lib/cloudinary';
-import { getDatabase } from '@/lib/db';
-import { BookChunkDb, BookDb } from '@/types/Books';
-import { openai } from '@ai-sdk/openai';
-import { embedMany } from 'ai';
-import { Db, ObjectId } from 'mongodb';
+import { generateMd } from '@/ai/utils/books'
+import { uploadFile } from '@/lib/cloudinary'
+import { getDatabase } from '@/lib/db'
+import { BookChunkDb, BookDb } from '@/types/Books'
+import { openai } from '@ai-sdk/openai'
+import { embedMany } from 'ai'
+import { Db, ObjectId } from 'mongodb'
 import { NextRequest, NextResponse } from 'next/server'
-import { PDFDocument } from 'pdf-lib';
-import { Resend } from 'resend';
+import { PDFDocument } from 'pdf-lib'
+import { Resend } from 'resend'
 
 const IMAGE_URL = (publicId: string, page: number = 1) =>
 	`https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload/pg_${page}/q_auto/${publicId}.jpeg`
@@ -33,7 +33,7 @@ const getCunks = async (publicId: string, pages: number) => {
 		totalPricing += pricing
 	}
 
-    const PESO_MXN = 20.55
+	const PESO_MXN = 20.55
 	console.log(`-------------------------------
 		Total Cost: $${totalPricing} - $MXN ${totalPricing * PESO_MXN * 1.03}`)
 
@@ -41,24 +41,24 @@ const getCunks = async (publicId: string, pages: number) => {
 }
 
 const saveEmbeddings = async (bookChunks: BookChunkDb[], db: Db) => {
-    const embeddingCollection = db.collection<BookChunkDb>('book_chunks')
-    const result = await embeddingCollection.insertMany(bookChunks)
+	const embeddingCollection = db.collection<BookChunkDb>('book_chunks')
+	const result = await embeddingCollection.insertMany(bookChunks)
 
-    return result
+	return result
 }
 
-const processBook = async (buffer: string | Buffer, bookId: ObjectId, fileName: string) => {
-	const pdfDoc = await PDFDocument.load(buffer)
-	const pages = pdfDoc.getPageCount()		
-
-	const publicId = await uploadFile(buffer, 'application/pdf')
-
-	const chunks = (await getCunks(publicId, pages)).filter((e) => e)
+const processBook = async (
+	bookId: ObjectId,
+	cloudinaryId: string,
+	pages: number,
+	fileName: string
+) => {
+	const chunks = (await getCunks(cloudinaryId, pages)).filter((e) => e)
 
 	const { embeddings } = await embedMany({
 		model: openai.embedding('text-embedding-3-large'),
 		values: chunks.map((chunk) => chunk.content),
-	})	
+	})
 
 	const bookChunks = embeddings.map((embedding, i) => ({
 		...chunks[i],
@@ -70,7 +70,7 @@ const processBook = async (buffer: string | Buffer, bookId: ObjectId, fileName: 
 	const db = await getDatabase()
 
 	const embeddingResult = await saveEmbeddings(bookChunks, db)
-	
+
 	const bookCollection = db.collection<BookDb>('books')
 
 	const result = await bookCollection.insertOne({
@@ -83,7 +83,6 @@ const processBook = async (buffer: string | Buffer, bookId: ObjectId, fileName: 
 	})
 
 	if (result.insertedId) {
-		
 		const resend = new Resend(process.env.RESEND_TOKEN)
 
 		await resend.emails.send({
@@ -92,41 +91,21 @@ const processBook = async (buffer: string | Buffer, bookId: ObjectId, fileName: 
 			subject: 'Vectorizado de PDF terminado',
 			html: `<p>Tu archivo <strong>${fileName}.pdf</strong> ha terminado de vectorizarse, id: <strong>${bookId}</strong> </p>`,
 		})
-
-		
 	} else throw Error('Server error')
 }
 
+type BodyData = { pages: number; cloudinaryId: string; fileName: string }
+
 export async function POST(request: NextRequest) {
 	try {
-		const formData = await request.formData() // Extrae el FormData del request
-		const file = formData.get('file') as File | null
-
-		if (!file) {
-			return NextResponse.json({ error: 'No se recibió ningún archivo' }, { status: 400 })
-		}
-
-		// Validar la extensión y el tipo MIME
-		if (!file.name.endsWith('.pdf') || file.type !== 'application/pdf') {
-			return NextResponse.json(
-				{ error: 'Solo se permiten archivos PDF' },
-				{ status: 400 }
-			)
-		}
-		
-
-		// Leer el contenido del archivo
-		const bytes = await file.arrayBuffer()
-		const buffer = Buffer.from(bytes)
-
 		const bookId = new ObjectId()
+		const { pages, cloudinaryId, fileName }: BodyData = await request.json()
 
-		processBook(buffer, bookId, file.name)
+		processBook(bookId, cloudinaryId, pages, fileName)
 
 		return NextResponse.json({
 			msg: 'Processing book... you will notified at your email',
-			bookId: bookId
+			bookId: bookId,
 		})
-
 	} catch (error) {}
 }
--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -1,4 +1,4 @@
-import { v2 as cloudinary } from 'cloudinary'
+import { v2 as cloudinary, SignApiOptions, UploadApiResponse } from 'cloudinary'
 
 
 cloudinary.config({
@@ -11,8 +11,12 @@ cloudinary.config({
 const uploadFile = async (data: string | Buffer, mimetype = 'image/png') => {
     try {
         const result = await cloudinary.uploader.upload(
-            `data:${mimetype};base64,${data}`
+            `data:${mimetype};base64,${data}`, {
+                
+            }
         )
+
+        cloudinary.uploader.unsigned_upload
 
         return result.public_id
     } catch (error) {
@@ -20,6 +24,68 @@ const uploadFile = async (data: string | Buffer, mimetype = 'image/png') => {
         return ''
     }
 }
+export async function uploadFileNew(file: File, signApiUrl: string) {
+    const timestamp = Date.now();
+    const formData = new FormData();
+    const parameters: SignApiOptions = {};
+    const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
+    const uploadsFolder = process.env.NEXT_PUBLIC_CLOUDINARY_UPLOADS_FOLDER;
+  
+    if (!cloudName) {
+      throw new Error("Missing NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME");
+    }
+  
+    if (uploadsFolder) {
+      parameters.folder = uploadsFolder;
+    }
+  
+    Object.keys(parameters)
+      .sort()
+      .forEach(key => {
+        if (typeof parameters[key] === "undefined") return;
+        formData.append(key, String(parameters[key]));
+      });
+  
+    const { signature } = await fetch(signApiUrl, {
+      method: "POST",
+      body: JSON.stringify({
+        ...parameters,
+        timestamp: String(timestamp),
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(r => r.json());
+  
+    if (!signature) {
+      throw new Error("Failed to sign the request");
+    }
+  
+    formData.append("file", file);
+    formData.append("timestamp", String(timestamp));
+    formData.append("signature", signature);
+    formData.append(
+      "api_key",
+      String(process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY)
+    );
+  
+    const response = await fetch(
+      `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+  
+    if (!response.ok) {
+      const { error } = await response.json();
+      throw new Error(error.message || "Uknown error");
+    }
+  
+    const results: UploadApiResponse = await response.json();
+  
+    return results;
+  }
 
 export {
     cloudinary,
