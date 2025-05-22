938a4aa20f082a07db50631a62097610dccb2c3e
Eduardo <juantokx@gmail.com>
2025-05-21 19:03:37 -0600

    feat: refactor chat response handling and improve message processing logic

--- src/lib/chat.ts
+++ src/lib/chat.ts
@@ -1,20 +1,71 @@
 import { getAiPlugin } from '@/ai/plugins'
 import { ChatBot, ChatBotDb } from '@/types/ChatBot'
 import { openai } from '@ai-sdk/openai'
-import { CoreTool, Message, streamText } from 'ai'
+import {
+	CoreTool,
+	Message,
+	streamText,
+	generateText,
+	LanguageModelResponseMetadata,
+	CoreAssistantMessage,
+	CoreToolMessage,
+	LanguageModelUsage,
+} from 'ai'
 import { ObjectId, WithId } from 'mongodb'
 import { getDatabase } from './db'
 import { ChatDb } from '@/types/Chat'
+
+type ChatResponse = LanguageModelResponseMetadata & {
+	readonly messages: Array<CoreAssistantMessage | CoreToolMessage>
+}
+
+const processFinish = async (
+	chatBot: WithId<ChatBot>,
+	chatId: ObjectId,
+	{
+		text,
+		response,
+		usage,
+	}: { text: string; response: ChatResponse; usage: LanguageModelUsage }
+) => {
+	const db = await getDatabase()
+	const chatCollection = db.collection<ChatDb>('chat')
+	const chatBotCollection = db.collection<ChatBotDb>('chatbot')
+	console.log({ text })
+	await chatCollection.updateOne(
+		{ _id: chatId },
+		{
+			$push: {
+				messages: {
+					role: 'assistant',
+					content: text,
+					// @ts-ignore
+					id: response.messages[response.messages.length - 1].id,
+				},
+			},
+		}
+	)
+	await chatBotCollection.updateOne(
+		{
+			_id: chatBot._id,
+		},
+		{
+			$inc: {
+				totalMessages: 1,
+				'usedTokens.input': usage.promptTokens,
+				'usedTokens.output': usage.completionTokens,
+			},
+		}
+	)
+}
 
 export const generateResponse = async (
 	chatBot: WithId<ChatBot>,
 	messages: Message[],
 	chatId: ObjectId,
-    stream = true
+	stream = true
 ) => {
 	const db = await getDatabase()
-	const chatCollection = db.collection<ChatDb>('chat')
-	const chatBotCollection = db.collection<ChatBotDb>('chatbot')
 
 	const { model, initialPrompt, name, tools: toolObjects } = chatBot
 
@@ -25,41 +76,29 @@ export const generateResponse = async (
 		const tool = getAiPlugin(toolObject.id, toolObject.settings)
 		tools[toolObject.id] = tool
 	}
-	const result = streamText({
-		model: openai(model),
-		system: `${name} - ${initialPrompt}`,
-		// Only use last 8 messages:
-		messages: messages.slice(-8),
-		onFinish: async ({ text, response, usage }) => {
-			console.log({ text })
-			await chatCollection.updateOne(
-				{ _id: chatId },
-				{
-					$push: {
-						messages: {
-							role: 'assistant',
-							content: text,
-							id: response.messages[response.messages.length - 1].id,
-						},
-					},
-				}
-			)
-			await chatBotCollection.updateOne(
-				{
-					_id: chatBot._id,
-				},
-				{
-					$inc: {
-						totalMessages: 1,
-						'usedTokens.input': usage.promptTokens,
-						'usedTokens.output': usage.completionTokens,
-					},
-				}
-			)
-		},
-		tools,
-		maxSteps: 3,
-	})
 
-    return stream ? result.toDataStreamResponse() : result.text
+	if (stream) {
+		const result = streamText({
+			model: openai(model),
+			system: `${name} - ${initialPrompt}`,
+			// Only use last 8 messages:
+			messages: messages.slice(-8),
+			onFinish: (data) => processFinish(chatBot, chatId, data),
+			tools,
+			maxSteps: 3,
+		})
+		return result.toDataStreamResponse()
+	} else {
+		const { text, usage, response } = await generateText({
+			model: openai(model),
+			system: `${name} - ${initialPrompt}`,
+			// Only use last 8 messages:
+			messages: messages.slice(-8),
+			tools,
+			maxSteps: 3,
+		})
+		await processFinish(chatBot, chatId, { text, usage, response })
+
+		return  text
+	}
 }
