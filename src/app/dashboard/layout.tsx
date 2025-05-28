83f5108555dac4b5ed652fa30fedb8de27db8ab5
Eduardo <juantokx@gmail.com>
2025-05-28 11:24:32 -0600

    feat: add DashboardProvider for React Query integration and update layout to use it

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,24 +2,30 @@ import { auth } from '@/auth'
 import { AppSidebar } from '@/components/app-sidebar'
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 import { redirect } from 'next/navigation'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { DashboardProvider } from './DashboardProvider'
+
 
 export default async function NewDashboardLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode
 }>) {
-
-    const session = await auth()
-    if (!session?.user || !session.user.id) return redirect('/')
+	const session = await auth()
+	if (!session?.user || !session.user.id) return redirect('/')
 
 	return (
-		<SidebarProvider >
-			<AppSidebar user={{
-                name: session.user.name || '',
-                avatar: session.user.image || '',
-                email: session.user.email || '',
-            }} />
-			<SidebarInset>{children}</SidebarInset>
-		</SidebarProvider>
+		<DashboardProvider>
+			<SidebarProvider>
+				<AppSidebar
+					user={{
+						name: session.user.name || '',
+						avatar: session.user.image || '',
+						email: session.user.email || '',
+					}}
+				/>
+				<SidebarInset>{children}</SidebarInset>
+			</SidebarProvider>
+		</DashboardProvider>
 	)
 }
